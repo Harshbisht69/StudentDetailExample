32048e61286c226785fc74602271e6ea50c5be00
harsh <harsh@HARSH>
2024-05-03 13:07:58 +0530

    Add project files.

---
+++ Controllers/StudentDetailController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentDetail.Model;
+using static StudentDetail.Model.StudentDetailModel;
+
+namespace StudentDetail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentDetailController : ControllerBase
+    {
+
+        [HttpPost]
+        public string SaveStudentDetails(StudentDetailProperties studentDetailPropertyObj)
+        {
+            StudentDetailModel.StudentDetailOperations studentDetailObj = new StudentDetailModel.StudentDetailOperations();
+            studentDetailObj.SaveStudentDetails(studentDetailPropertyObj);
+            return "success";
+        }
+    }
+}
---
+++ Model/StudentDetailModel.cs
@@ -0,0 +1,41 @@
+﻿using StudentDetailBL.Shared;
+
+namespace StudentDetail.Model
+{
+    public class StudentDetailModel
+    {
+        public class StudentDetailOperations
+        {
+            public string SaveStudentDetails(StudentDetailProperties studentDetailPropertyObj)
+            {
+                StudentDetailBL.Contracts.Request.StudentDetailRequest studentRequestObj = new StudentDetailBL.Contracts.Request.StudentDetailRequest();
+
+                studentRequestObj.address = studentDetailPropertyObj.address;
+                studentRequestObj.studentName = studentDetailPropertyObj.studentName;
+                studentRequestObj.rollNumber = studentDetailPropertyObj.rollNumber;
+
+                studentRequestObj.BehaviourAction = "insert_student_details";
+                studentRequestObj.BehaviourClass = "StudentDetailBL.Behaviour.StudentDetailBehaviour";
+
+                //StudentDetailBL.Behaviour.StudentDetailBehaviour studentBehObj = new StudentDetailBL.Behaviour.StudentDetailBehaviour();
+                //studentBehObj.SaveStudentDetails(studentRequestObj);
+
+                ServiceLocator serviceLocatorOb = new ServiceLocator();
+                serviceLocatorOb.Execute(studentRequestObj);
+
+                return "success";
+            }
+        }
+
+        public class StudentDetailProperties
+        {
+            public string studentName { get; set; }
+            public string address { get; set; }
+            public string rollNumber { get; set; }
+        }
+        
+    }
+
+    
+
+}
